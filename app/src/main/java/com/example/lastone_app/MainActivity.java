5f4ed3c3951a17400a0ad4c534ccd316a0bc1ecb
jeongho <pjhpjh0805@naver.com>
2023-10-30 16:19:30 +0900

    feat: 겔러리 사진 업로드 기능 구현

--- app/src/main/java/com/example/lastone_app/MainActivity.java
+++ app/src/main/java/com/example/lastone_app/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.lastone_app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+    private ValueCallback<Uri[]> uploadMessage;
+    private final ActivityResultLauncher<Intent> fileChooserLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (uploadMessage == null) return;
+                Uri[] results = WebChromeClient.FileChooserParams.parseResult(result.getResultCode(), result.getData());
+                uploadMessage.onReceiveValue(results);
+                uploadMessage = null;
+            });
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,9 +39,22 @@ public class MainActivity extends AppCompatActivity {
         webView = findViewById(R.id.webview);
 
         webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                uploadMessage = filePathCallback;
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("*/*");
+                fileChooserLauncher.launch(Intent.createChooser(intent, "File Browser"));
+                return true;
+            }
+        });
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
         webSettings.setUseWideViewPort(true);
         webSettings.setLoadWithOverviewMode(true);
 
